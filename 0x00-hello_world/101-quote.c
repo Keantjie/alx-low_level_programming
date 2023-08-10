bdc0d67ca18eeab049289297f128d0bb6645c7ea
Keantjie <keamom97@gmail.com>
2023-08-10 11:19:03 -0700

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - Entry point for execution
+ *
+ * Return: always 0 if successful
+ */
+int main(void)
+{
+	write(2, "nd that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
