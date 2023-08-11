259103400623a0266ad66ac02f026c1c4777f588
Keantjie <keamom97@gmail.com>
2023-08-11 07:26:20 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
- * main - Prints numbers of base 16
+ * main - Prints variables of base 16
  *
  * Return: Always 0
  */
 int main(void)
 {
-	char letter;
 	int num;
+	char letter;
 
-	for (letter = 'a'; letter <= 'f'; letter++)
-		putchar(letter);
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
 	putchar('\n');
 	return (0);
 }
