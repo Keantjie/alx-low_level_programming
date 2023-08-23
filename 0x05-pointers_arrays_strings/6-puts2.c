ab1cde34d829f0d92baa09908dbd90fbcfe6bb03
Keantjie <keamom97@gmail.com>
2023-08-23 05:37:19 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints only one char from 2 strings
+ * @str: string to be updated
+ */
+void puts2(char *str)
+{
+	int k, i;
+
+	k = 0;
+	while (str[k] != '\0')
+	{
+		k++;
+	}
+	for (i = 0; i < k; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
