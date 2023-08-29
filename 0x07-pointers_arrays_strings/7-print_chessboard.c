02299e865e8e1e6778bd5a4796021382f8cdd8fc
Keantjie <keamom97@gmail.com>
2023-08-29 13:09:01 -0700

    print_chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,12 +5,14 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int d, b;
+	int i, j;
 
-	for (d = 0; d < 8; d++)
+	for (i = 0; i < 8; i++)
 	{
-		for (b = 0; b < 8; b++)
-			_putchar(a[d][b]);
-			_putchar('\n');
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
 	}
 }
